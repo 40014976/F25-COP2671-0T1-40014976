110acbf6a0d4dd951535c95746ab323a1d33e20d
Ethan Tang <40014976@sfcollege.edu>
2025-10-09 04:40:54 -0400

    completed Lesson 5.2

--- Units/User Interface/Prototype 5/Assets/Scripts/GameManager.cs
+++ Units/User Interface/Prototype 5/Assets/Scripts/GameManager.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     private float spawnRate = 1;
+    private int score;
+    public TextMeshProUGUI scoreText;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        score = 0;
     }
 
     // Update is called once per frame
@@ -28,5 +32,11 @@ public class GameManager : MonoBehaviour
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
         }
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
     }
 }
